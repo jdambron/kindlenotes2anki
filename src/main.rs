ec53cb737a5efe34c7f8d0aab54acbe322fccb5f
jdambron <jdambron@users.noreply.github.com>
2020-07-20 13:38:48 +0200

    add some unit tests

--- src/main.rs
+++ src/main.rs
@@ -54,3 +54,48 @@ fn is_useless_line(line: &str) -> bool {
         || line.starts_with("- Votre note")
         || line.is_empty()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn surlignement_is_useless() {
+        assert!(is_useless_line("- Votre surlignement Emplacement 1212-1214 | Ajouté le samedi 20 octobre 2018 à 12:55:45"));
+    }
+
+    #[test]
+    fn signet_is_useless() {
+        assert!(is_useless_line(
+            "- Votre signet Emplacement 5527 | Ajouté le vendredi 16 novembre 2018 à 11:51:19"
+        ));
+    }
+
+    #[test]
+    fn note_is_useless() {
+        assert!(is_useless_line(
+            "- Votre note Emplacement 3752 | Ajoutée le vendredi 16 novembre 2018 à 13:51:19"
+        ));
+    }
+
+    #[test]
+    fn empty_is_useless() {
+        assert!(is_useless_line(""));
+    }
+
+    #[test]
+    fn highlight_is_useful() {
+        assert!(!is_useless_line("A standard fake highlight"));
+    }
+
+    #[test]
+    fn test_parse_note() {
+        let fake_note = "A fake title (Last, First)\n- Votre surlignement Emplacement 3592-3592 | Ajouté le mardi 6 novembre 2018 à 08:50:39\n\nThis is a fake highlight.\n";
+        if let Some((title, tidied_note)) = parse_note(fake_note) {
+            assert_eq!(title, "A fake title (Last, First)");
+            assert_eq!(tidied_note, "This is a fake highlight.");
+        } else {
+            panic!("The parsed note should not be empty");
+        }
+    }
+}
