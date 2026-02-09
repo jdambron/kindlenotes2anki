581abc5c40182dbd8d96f06902d94123f987efd4
Julien Dambron <dreamlike3d@gmail.com>
2026-02-09 07:14:44 +0100

    fix: safe error handling in CVS writer

--- src/csv_writer.rs
+++ src/csv_writer.rs
@@ -5,7 +5,7 @@ use std::io;
 pub fn write_csv(notes: Vec<Note>) -> Result<()> {
     let mut wtr = csv::Writer::from_writer(io::stdout());
     for note in notes {
-        wtr.write_record(&[note.title, note.tidied_note]).unwrap();
+        wtr.write_record(&[note.title, note.tidied_note])?;
     }
     wtr.flush()?;
     Ok(())
